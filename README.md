4bacff9ed576bdd30f5834cd7050c19fda55a81c
Franco D. Berdun <fberdun@gmail.com>
2019-04-25 15:45:47 -0300

    test chai

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ciclo
+
+
+1- init Node app
+      node index.js
+2- config Travis CI
+      git add .
+      git commit -m ""
+      git push origin master
+3- install test package
+      npm install mocha — save-dev
+      npm install chai — save-dev
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+'use strict';
 
-var handle = {}
-handle["/"] = requestHandlers.inicio;
-handle["/iniciar"] = requestHandlers.iniciar;
-handle["/subir"] = requestHandlers.subir;
+var calculator = require('./app/calculator');
+var a = 3;
+var b = 5;
 
-server.wakeup(router.route, handle);
+console.log(`Add ${a} + ${b} = ${calculator.add(a,b)}`);
+console.log(`Minus ${a} + ${b} = ${calculator.minus(a,b)}`);
+console.log(`Multiply ${a} + ${b} = ${calculator.multiply(a,b)}`);
+console.log(`Divide ${a} + ${b} = ${calculator.divide(a,b)}`);
+
+// var server = require("./server");
+// var router = require("./router");
+// var requestHandlers = require("./requestHandlers");
+//
+// var handle = {}
+// handle["/"] = requestHandlers.inicio;
+// handle["/iniciar"] = requestHandlers.iniciar;
+// handle["/subir"] = requestHandlers.subir;
+//
+// server.wakeup(router.route, handle);
